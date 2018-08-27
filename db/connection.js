93fe1d125afbdf440a8cb5bfdf7e461efb0199cc
Angela Malone <a.malone1193@gmail.com>
2018-08-27 16:47:37 -0400

    new url parser

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
 
 if (process.env.NODE_ENV == "production") {
-  mongoose.connect(process.env.MLAB_URL);
+  mongoose.connect(
+    process.env.MLAB_URL,
+    { useNewUrlParser: true }
+  );
 } else {
-  mongoose.connect("mongodb://localhost/api-tips");
+  mongoose.connect(
+    "mongodb://localhost/api-tips",
+    { useNewUrlParser: true }
+  );
 }
 mongoose.Promise = Promise;
 module.exports = mongoose;
